172938bf0d37e49636579a55a5aa486b5c79ae57
adriandotdev <marcelonads@gmail.com>
2026-05-03 19:54:09 +0800

    add linting on CI

--- src/middleware/morgan.ts
+++ src/middleware/morgan.ts
@@ -1,9 +1,10 @@
+import type { Request } from "express";
 import morgan from "morgan";
 
 morgan.token("type", (req) => req.headers["content-type"]);
 morgan.token("host", (req) => req.headers["host"]);
 morgan.token("realIp", (req) => req.headers["x-real-ip"] as string);
-morgan.token("body", (req) => JSON.stringify((req as any).body));
+morgan.token("body", (req) => JSON.stringify((req as Request).body));
 
 export const BASE_FORMAT = ":host :realIp :type :method :url :status";
 export const morganLogger = morgan(BASE_FORMAT);
